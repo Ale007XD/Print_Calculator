361324f233d3bd9662639db96ecfd2760574aad2
Ale007XD <alex.deloverov@gmail.com>
2025-07-24 17:56:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,54 @@
+const CACHE_NAME = 'print-calculator-v1';
+const URLS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/manifest.json',
+    '/lib/jspdf.umd.min.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
 
+// Установка Service Worker и кэширование всех ресурсов
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(URLS_TO_CACHE);
+            })
+    );
+});
+
+// Активация Service Worker и удаление старых кэшей
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+// Перехват сетевых запросов
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Если ресурс есть в кэше, возвращаем его
+                if (response) {
+                    return response;
+                }
+                // Иначе, выполняем сетевой запрос
+                return fetch(event.request);
+            })
+    );
+});
