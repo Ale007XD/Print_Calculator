1e06d1d5c7a69f4ee4da586d8061e935c9d4fecd
Ale007XD <alex.deloverov@gmail.com>
2025-07-25 10:45:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'print-calculator-v8'; // ВАЖНО: Смените номер версии
+const CACHE_NAME = 'print-calculator-v8'; // Новая версия для обновления
 
 const URLS_TO_CACHE = [
     '/Print_Calculator/',
@@ -6,48 +6,24 @@ const URLS_TO_CACHE = [
     '/Print_Calculator/style.css',
     '/Print_Calculator/app.js',
     '/Print_Calculator/manifest.json',
-    '/Print_Calculator/lib/jspdf.umd.min.js'
+    '/Print_Calculator/lib/jspdf.umd.min.js',
+    '/Print_Calculator/icons/icon-192x192.png',
+    '/Print_Calculator/icons/icon-512x512.png'
 ];
 
-// Установка Service Worker и кэширование всех ресурсов
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(URLS_TO_CACHE);
-            })
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
 });
 
-// Активация Service Worker и удаление старых кэшей
 self.addEventListener('activate', event => {
     const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName); // Удаляем старые кэши, например 'print-calculator-v1'
-                    }
-                })
-            );
-        })
-    );
+    event.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName => {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+        }
+    }))));
 });
 
-
-// Перехват сетевых запросов
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Если ресурс есть в кэше, возвращаем его
-                if (response) {
-                    return response;
-                }
-                // Иначе, выполняем сетевой запрос
-                return fetch(event.request);
-            })
-    );
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
